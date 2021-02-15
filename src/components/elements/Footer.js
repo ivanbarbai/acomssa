c1981480e0fc1789c43620acad989bc4e3daf090
Ivan Barba <ivanbarba@canesstudio.com>
2021-02-15 17:11:13 -0600

    redirect added

--- src/components/elements/Footer.js
+++ src/components/elements/Footer.js
@@ -1,17 +1,34 @@
 import React from 'react'
-import { Footer, Text, Box,  } from 'grommet'
+import { Footer, Text, Box, Anchor, Grommet, grommet, } from 'grommet'
+import { deepMerge } from 'grommet/utils'
+
+const customAnchor = deepMerge(grommet, {
+    anchor: {
+        textDecoration: 'none',
+        fontWeight: 700,
+        hover: {
+            textDecoration: 'none',
+        },
+        color:'#ffffff',
+    },
+    text:{
+        color:'#ffffff'
+    }
+})
 
 const Footed = () => {
     return (
-        <Footer background="dark-5" pad={{ horizontal: 'large', vertical: 'small' }}>
-            <Box 
-            direction="row" 
-            gap="small"
-            >
-                <Text alignSelf="center">Acomssa</Text>
-            </Box>
-            <Text textAlign="center" size="small">© 2019 Copyright</Text>
-        </Footer>
+        <Grommet theme={customAnchor}>
+            <Footer background="#5966F0" pad={{ horizontal: 'large', vertical: 'small' }}>
+                <Box
+                    direction="row"
+                    gap="small"
+                >
+                    <Anchor href='/' alignSelf="center">Acomssa</Anchor>
+                </Box>
+                <Text textAlign="center" size="small">© 2019 Copyright</Text>
+            </Footer>
+        </Grommet>
     )
 }
 
